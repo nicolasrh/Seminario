47200c3c3782dc8b63d5c438b0d583fa4ab3cebd
nicolas rodriguez hernandez <nicolasakisora@outlook.es>
2020-06-25 20:37:28 -0500

    Add team controller

---
+++ Soccer.Web/Data/Datacontext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Soccer.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Soccer.Web.Data
+{
+    public class Datacontext :DbContext 
+    {
+        public Datacontext(DbContextOptions<Datacontext> options) : base(options)
+        {
+        }
+        public DbSet<TeamEntity> Teams { get; set; }
+    }
+}
+ 
